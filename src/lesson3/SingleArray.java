918ff424bf524aa1a383f128eaf6f84537ad29de
Slava Vasileuskiy <itreyserpwnz@gmail.com>
2024-03-09 23:30:19 +0300

    выполнение домашики 3

---
+++ src/lesson3/SingleArray.java
@@ -0,0 +1,151 @@
+package lesson3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class SingleArray {
+
+
+    public static void searchInArray() {
+        int[] array = new int[10];
+        Random r = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = r.nextInt();
+        }
+        Scanner scan = new Scanner(System.in);
+        System.out.println(Arrays.toString(array));
+        System.out.println("Введите число, которое хотите найти в массиве: ");
+        int input = scan.nextInt();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == input) {
+                System.out.println("Ваше число найдено в массиве!");
+                break;
+            } else if (i == array.length - 1) {
+                System.out.println("Ваше число не найдено в массиве!");
+            }
+        }
+    }
+
+    public static void removeInArray() {
+        int[] array = new int[20];
+        Random r = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = r.nextInt();
+        }
+        Scanner scan = new Scanner(System.in);
+        System.out.println(Arrays.toString(array));
+        System.out.println("Введите число, которое хотите удалить в массиве: ");
+        int input = scan.nextInt();
+        ArrayList<Integer> list = new ArrayList<>();
+        if ((Arrays.binarySearch(array, input)) >= 0) {
+            System.out.println("Такого значения нету в массиве!");
+        } else {
+            for (int j : array) {
+                if (j != input) {
+                    list.add(j);
+                }
+            }
+            int[] newArray = new int[list.size()];
+            for (int i = 0; i < list.size(); i++) {
+                newArray[i] = list.get(i);
+            }
+            System.out.println(Arrays.toString(newArray));
+        }
+    }
+
+    public static void createArrayAndFind() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите размер массива: ");
+        double[] array = new double[scan.nextInt()];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Math.random();
+        }
+        double min = array[0];
+        double max = array[0];
+        double counter = 0;
+        for (int i = 0; i < array.length; i++) {
+            counter += array[i];
+            if (min < array[i]) {
+                min = array[i];
+            }
+            if (max > array[i]) {
+                max = array[i];
+            }
+        }
+        System.out.printf("Максимальное значение: %f, минимальное значение %f и среднее значением - %f", max, min, counter / array.length);
+    }
+
+    public static void createTwoArrays() {
+        int[] array1 = new int[5];
+        int[] array2 = new int[5];
+        Random r = new Random();
+        for (int i = 0; i < 5; i++) {
+            array1[i] = r.nextInt();
+            array2[i] = r.nextInt();
+        }
+        double counter1 = 0;
+        double counter2 = 0;
+        for (int i = 0; i < 5; i++) {
+            counter1 = array1[i];
+            counter2 = array2[i];
+        }
+        double res1 = counter1 / 5;
+        double res2 = counter2 / 5;
+        System.out.println(Arrays.toString(array1));
+        System.out.println(Arrays.toString(array2));
+        if (res1 > res2) {
+            System.out.println("Среднее арифмитическое значение суммы значений массива 1 больше");
+        } else if (res1 < res2) {
+            System.out.println("Среднее арифмитическое значение суммы значений массива 2 больше");
+        } else {
+            System.out.println("Их сердне арифметические значения равны");
+        }
+    }
+
+    public static void singleArray() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите размер массива: ");
+        int size = scan.nextInt();
+        while (size < 6 || size > 10) { // проверка, что ввели размер массива в интервале от 6 до 10
+            System.out.println("Размер массива должен быть от 6 и до 10 включительно");
+            size = scan.nextInt();
+        }
+        int[] array = new int[size]; // создание массива
+        Random r = new Random();
+        for (int i = 0; i < array.length; i++) { // заполнение массива рандомом
+            array[i] = r.nextInt();
+        }
+        System.out.println(Arrays.toString(array));
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int j : array) { // в созданный список засовываем положительные значения
+            if (j > 0) {
+                list.add(j);
+            }
+        }
+        int[] newArray = new int[list.size()]; // новый массив с размерностью списка
+        for (int i = 0; i < list.size(); i++) { // засовываем в массив значния из списка
+            newArray[i] = list.get(i);
+        }
+        System.out.println(Arrays.toString(newArray));
+    }
+
+    public static void createAndReplaceInArray() {
+        int[] array = new int[10];
+        Random r = new Random();
+        for (int i = 0; i < 10; i++) {
+            array[i] = r.nextInt();
+        }
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        for (int i = 1; i < array.length; i += 2) {
+            array[i] = 0;
+        }
+        System.out.println();
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
+}
