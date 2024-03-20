76d85b68ac6711fe73a0695d344b3158d1387fe5
Slava Vasileuskiy <itreyserpwnz@gmail.com>
2024-03-20 22:19:51 +0300

    оптимизация создания фигур в дз7

--- src/lesson7/Main.java
+++ src/lesson7/Main.java
@@ -7,23 +7,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Figure[] figure = new Figure[5];
-        System.out.println("Введите радиус круга: ");
-        double radius = scan.nextDouble();
-        figure[0] = new Circle(radius);
-        System.out.println("Введите сторону квадрата: ");
-        double side = scan.nextDouble();
-        figure[1] = new Square(side);
-        System.out.println("Введите ширину прямоудольника: ");
-        double width = scan.nextDouble();
-        System.out.println("Введите высоту прямоугольника: ");
-        double height = scan.nextDouble();
-        figure[2] = new Rectangle(width, height);
+
+        for (int i = 0; i < figure.length; i++) {
+            System.out.println("Введите номер фигуры, которую хотите записать в массив : 1 - Круг, 2 - Прямоугольник, 3 - Квадрат");
+            int choice = scan.nextInt();
+            if (choice == 1) {
+                System.out.println("Введите радиус круга: ");
+                double radius = scan.nextDouble();
+                figure[i] = new Circle(radius);
+            } else if (choice == 2) {
+                System.out.println("Введите ширину прямоудольника: ");
+                double width = scan.nextDouble();
+                System.out.println("Введите высоту прямоугольника: ");
+                double height = scan.nextDouble();
+                figure[i] = new Rectangle(width, height);
+            }
+            else{
+                System.out.println("Введите сторону квадрата: ");
+                double side = scan.nextDouble();
+                figure[i] = new Square(side);
+            }
+        }
 
         double totalPerimeter = 0;
         for (Figure i : figure) {
-            if (i == null) {
-                continue;
-            }
             totalPerimeter += i.perimeter();
         }
 
